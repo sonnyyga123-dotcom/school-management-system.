d4780b519454be25a3120ec322a516bc28689284
sonnyyga123-dotcom <sonnyyga123@gmail.com>
2026-03-12 20:03:05 +0530

    Add files via upload

---
+++ autodemo.java
@@ -0,0 +1,21 @@
+class autodemo
+{
+public static void main(String a[])
+ {
+  int A=10;
+  Integer i=A;
+  System.out.println("primitive a is:"+A);
+  System.out.println("Wrapper class object is:"+i);
+ }
+}
+class unboxdemo
+{
+public static void main(String a[])
+ {
+  Integer i=10;
+  int A=i;
+  System.out.println("primitive a is:"+A);
+  System.out.println("Wrapper class object is:"+i);
+ }
+}
+
